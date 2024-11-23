5dbd14d2c224d5a569705aeb6a8eb50410f1c2bb
Pablo-sx <pablowy.adres@gmail.com>
2024-11-23 22:37:57 +0100

    Poprawki

--- Spotify/try.py
+++ Spotify/try.py
@@ -1,15 +1,39 @@
 import tkinter as tk
 
-root=tk.Tk()
+# Tworzenie głównego okna
+root = tk.Tk()
 root.geometry("800x600")
+root.config(bg="gray")
 
-images_side_bar=tk.PhotoImage(file='Img/icon.png')
+def changing_color(label):
+    # Funkcja zmienia kolor tła przekazanej etykiety
+    label.config(bg='white')
 
-side_menu_frame= tk.Frame(root, bg="#383838", width="45")
-icon_btn=tk.Button(side_menu_frame, bg="#383838", image=images_side_bar, bd=0)
+# Wczytywanie obrazków
+image_side_bar = tk.PhotoImage(file='Img/icon.png')
+image_home = tk.PhotoImage(file='Img/home.png')
+side_bar_color = '#383838'
 
+# Tworzenie paska bocznego
+side_bar = tk.Frame(root, bg=side_bar_color, width=45)
 
-side_menu_frame.pack(side=tk.LEFT, fill="y", pady=3, padx=3)
-icon_btn.place(x=4, y=4)
+# Tworzenie przycisków i etykiety
+side_bar_btn = tk.Button(side_bar, bg=side_bar_color, image=image_side_bar, bd=0, activebackground=side_bar_color)
+home_btn = tk.Button(
+    side_bar, 
+    bg=side_bar_color, 
+    image=image_home, 
+    bd=0, 
+    activebackground=side_bar_color, 
+    command=lambda: changing_color(home_side_place)  # Przekazanie obiektu etykiety
+)
+home_side_place = tk.Label(side_bar, bg=side_bar_color)
 
+# Ustawianie elementów na pasku bocznym
+side_bar.pack(side=tk.LEFT, fill="y", pady=3, padx=3)
+side_bar_btn.place(x=4, y=4)
+home_btn.place(x=9, y=130, width=30, height=40)  # Przycisk "home" niżej, aby się nie nachodził
+home_side_place.place(x=3, y=130, width=3, height=40)
+
+# Uruchomienie pętli głównej
 root.mainloop()
