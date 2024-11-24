59aee2ca60f216f11a5dba3463589017ef5cde9d
Pablo-sx <pablowy.adres@gmail.com>
2024-11-24 13:15:54 +0100

    side bar cały

--- Spotify/try.py
+++ Spotify/try.py
@@ -1,32 +1,80 @@
 import tkinter as tk
 
+#zmiana pasek pojawiający się przy ikonach w side bar
+def changing_color(label):
+    home_side_place.config(bg=side_bar_color)
+    library_side_place.config(bg=side_bar_color)
+    browse_side_place.config(bg=side_bar_color)
+    create_side_place.config(bg=side_bar_color)
+    liked_side_place.config(bg=side_bar_color)
+
+    label.config(bg='white')
+
 root = tk.Tk()
 root.geometry("800x600")
 root.config(bg="gray")
 
-def changing_color(label):
-    label.config(bg='white')
+#kolor dla side bar
+side_bar_color = '#383838'
 
+#obrazki dla ikon z side bar
 image_side_bar = tk.PhotoImage(file='Img/icon.png')
 image_home = tk.PhotoImage(file='Img/home.png')
-side_bar_color = '#383838'
+image_library= tk.PhotoImage(file='Img/library.png')
+image_browse=tk.PhotoImage(file='Img/browse.png')
+image_create=tk.PhotoImage(file='Img/create.png')
+image_liked=tk.PhotoImage(file='Img/liked.png')
 
 side_bar = tk.Frame(root, bg=side_bar_color, width=45)
 
+#przyciski w side bar
 side_bar_btn = tk.Button(side_bar, bg=side_bar_color, image=image_side_bar, bd=0, activebackground=side_bar_color)
-home_btn = tk.Button(
-    side_bar, 
-    bg=side_bar_color, 
-    image=image_home, 
-    bd=0, 
-    activebackground=side_bar_color, 
+home_btn = tk.Button(side_bar, bg=side_bar_color, image=image_home, 
+    bd=0, activebackground=side_bar_color, 
     command=lambda: changing_color(home_side_place)
 )
-home_side_place = tk.Label(side_bar, bg=side_bar_color)
+
+library_btn = tk.Button(side_bar, bg=side_bar_color, image=image_library, 
+    bd=0, activebackground=side_bar_color, 
+    command=lambda: changing_color(library_side_place)
+)
+
+browse_btn = tk.Button(side_bar, bg=side_bar_color, image=image_browse, 
+    bd=0, activebackground=side_bar_color, 
+    command=lambda: changing_color(browse_side_place)
+)
+
+create_btn = tk.Button(side_bar, bg=side_bar_color, image=image_create, 
+    bd=0, activebackground=side_bar_color, 
+    command=lambda: changing_color(create_side_place)
+)
+
+liked_btn = tk.Button(side_bar, bg=side_bar_color, image=image_liked, 
+    bd=0, activebackground=side_bar_color, 
+    command=lambda: changing_color(liked_side_place)
+)
+
+#pasek przy ikonach side bar
+home_side_place = tk.Label(side_bar, bg='white')
+library_side_place = tk.Label(side_bar, bg=side_bar_color)
+browse_side_place = tk.Label(side_bar, bg=side_bar_color)
+create_side_place = tk.Label(side_bar, bg=side_bar_color)
+liked_side_place = tk.Label(side_bar, bg=side_bar_color)
 
 side_bar.pack(side=tk.LEFT, fill="y", pady=3, padx=3)
+
 side_bar_btn.place(x=4, y=4)
+
 home_btn.place(x=9, y=130, width=30, height=40)
+library_btn.place(x=9, y=200, width=30, height=40)
+browse_btn.place(x=9, y=270, width=30, height=40)
+create_btn.place(x=9, y=340, width=30, height=40)
+liked_btn.place(x=9, y=410, width=30, height=40)
+
 home_side_place.place(x=3, y=130, width=3, height=40)
+library_side_place.place(x=3, y=200, width=3, height=40) 
+browse_side_place.place(x=3, y=270, width=3, height=40) 
+create_side_place.place(x=3, y=340, width=3, height=40) 
+liked_side_place.place(x=3, y=410, width=3, height=40) 
 
 root.mainloop()
